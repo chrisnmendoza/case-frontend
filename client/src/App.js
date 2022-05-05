73b6a0e29c46b36a1cc4af4b7af69e83c8beee95
root <root@DESKTOP-71OQ39S.localdomain>
2022-05-05 17:54:04 -0400

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+ 
+// We use Route in order to define the different routes of our application
+import { Route, Routes } from "react-router-dom";
+ 
+// We import all the components we need in our app
+import Navbar from "./components/navbar";
+import RecordList from "./components/recordList";
+import Edit from "./components/edit";
+import Search from "./components/search";
+ 
+const App = () => {
+ return (
+   <div>
+     <Navbar />
+     <Routes>
+       <Route exact path="/" element={<RecordList />} />
+       <Route path="/edit/:id" element={<Edit />} />
+       <Route path="/search" element={<Search />} />
+     </Routes>
+   </div>
+ );
+};
+ 
+export default App;
