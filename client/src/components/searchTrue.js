367e51a01ae539474d4ed54de7b9d887a61c1889
chrisnmendoza <mendozac@udel.edu>
2022-05-08 03:51:49 -0400

    ui is just about done

--- client/src/components/searchTrue.js
+++ client/src/components/searchTrue.js
@@ -3,7 +3,6 @@ import { useNavigate } from "react-router";
 import Multiselect from 'multiselect-react-dropdown';
  
 export default function Search() {
- let allLanguages = ['javascript','python','java','c#','php','html',"c++","css","sql","r",'c',"swift","ruby","xml","vba","typescript","bash","scala","powershell","matlab","kotlin","perl","dart","go","haskell","rust"]
  const [form, setForm] = useState({
    query: "",
    languages: "",
@@ -48,6 +47,11 @@ export default function Search() {
    selectedList.forEach(element => selectedLanguages += element.name + " ")
    updateForm({ languages: selectedLanguages})
  }
+
+ function changeCode(event) {
+   console.log(event.target.value)
+   updateForm({onlyCode: event.target.value})
+ }
  
  // This following section will display the form that takes the input from the user.
  return (
@@ -65,22 +69,18 @@ export default function Search() {
          />
        </div>
        <div className="form-group">
-         <label htmlFor="onlyCode">Only Code?</label>
-         <input
-           type="text"
-           className="form-control"
-           id="onlyCode"
-           value={form.onlyCode}
-           onChange={(e) => updateForm({ onlyCode: e.target.value })}
-         />
-       </div>
-       <div className="form-group">
-         <Multiselect
+         <label htmlFor="languages">Languages? (dont enter to search all)</label>
+         <Multiselect className="form-group"
          options={[{name: 'javascript'},{name: 'python'},{name: 'java'},{name: 'c-sharp'},{name: 'php'},{name: 'html'},{name: 'c++'},{name: 'css'},{name: 'sql'},{name: 'r'},{name: 'c'},{name: 'swift'},{name: 'ruby'},{name: 'xml'},{name: 'vba'},{name: 'typescript'},{name: 'bash'},{name: 'scala'},{name: 'powershell'},{name: 'matlab'},{name: 'kotlin'},{name: 'perl'},{name: 'dart'},{name: 'go'},{name: 'haskell'},{name: 'rust'}]} // Options to display in the dropdown
          onSelect={myFunc}//(updateForm({ languages: e[0].name})} // Function will trigger on select event
          onRemove={myFunc} // Function will trigger on remove event
          displayValue="name" // Property name to display in the dropdown options
          />
+       </div>
+       <div className="form-group" onChange={changeCode}>
+         <label htmlFor="onlyCode">Only Code?</label>
+         <input type="radio" value="yes" name="onlyCode" /> Yes
+         <input type="radio" value="no" name="onlyCode" /> No
        </div>
        <div className="form-group">
          <input
